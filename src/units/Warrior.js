426e5d71326e3a67193f4a3f3b53d1b997b656af
Kyrylo.Ih <kiril.ihnatchenko@gmail.com>
2026-04-08 21:59:46 +0300

    armor

--- src/units/Warrior.js
+++ src/units/Warrior.js
@@ -1,23 +1,37 @@
 import Unit from "./Unit.js";
 
 export default class Warrior extends Unit {
-    immunityCount = 0
+    immunityCount = 0;
+    armor = 5;
+
     constructor() {
         super("warrior", 10, 150);
     }
 
     takeDamage(damage) {
+        
         if (this.immunityCount === 0) {
+
+
+            damage = damage - this.armor;
+
+            if (damage < 0) {
+                damage = 0;
+            }
+
             super.takeDamage(damage);
-            this.ultimateProgress += damage/100
+            this.ultimateProgress += damage / 100;
+
         } else {
-            this.immunityCount--
-            console.log("ops, Im immune")
+            this.immunityCount--;
+            console.log("ops, Im immune");
         }
     }
 
     ultimate(team, enemies) {
         super.ultimate(team, enemies);
-        this.immunityCount = 3
+        this.immunityCount = 3;
     }
 }
+
+
