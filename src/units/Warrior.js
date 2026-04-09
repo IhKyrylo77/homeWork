56e8676bec76f10959a3b14a38c0d343584d8525
Kyrylo.Ih <kiril.ihnatchenko@gmail.com>
2026-04-09 22:45:41 +0300

    Revert "armor"
    This reverts commit 426e5d71326e3a67193f4a3f3b53d1b997b656af.

--- src/units/Warrior.js
+++ src/units/Warrior.js
@@ -1,37 +1,23 @@
 import Unit from "./Unit.js";
 
 export default class Warrior extends Unit {
-    immunityCount = 0;
-    armor = 5;
-
+    immunityCount = 0
     constructor() {
         super("warrior", 10, 150);
     }
 
     takeDamage(damage) {
-        
         if (this.immunityCount === 0) {
-
-
-            damage = damage - this.armor;
-
-            if (damage < 0) {
-                damage = 0;
-            }
-
             super.takeDamage(damage);
-            this.ultimateProgress += damage / 100;
-
+            this.ultimateProgress += damage/100
         } else {
-            this.immunityCount--;
-            console.log("ops, Im immune");
+            this.immunityCount--
+            console.log("ops, Im immune")
         }
     }
 
     ultimate(team, enemies) {
         super.ultimate(team, enemies);
-        this.immunityCount = 3;
+        this.immunityCount = 3
     }
 }
-
-
