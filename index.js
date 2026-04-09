483a705c3deea1caa673bee8e8603881970527e3
Balitsky <balitsky99@gmail.com>
2026-04-10 00:29:52 +0300

    update game loop

--- index.js
+++ index.js
@@ -1,57 +1,79 @@
+import inquirer from 'inquirer'
 import UnitGenerator from "./src/UnitGenerator.js";
+import boxen from "boxen";
 
 let unitGenerator = new UnitGenerator()
 
 let team1 = []
 let team2 = []
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 4; i++) {
     let member1 = unitGenerator.createRandomUnit()
     let member2 = unitGenerator.createRandomUnit()
     team1.push(member1)
     team2.push(member2)
 }
 
-
-// MAIN GAME CYCLE
-while (team1.length && team2.length) {
-    // console.clear()
-    console.log('-----------------------------')
-    nextRound()
-    console.table(team1)
-    console.table(team2)
-    await new Promise(resolve => setTimeout(resolve, 1000))
-}
-console.log(`CONGRATULATIONS!!! Team ${team1.length ? "1" : "2"} Won!`)
-
-function nextRound() {
-    let idxTeam1 = 0;
-    let idxTeam2 = 0;
-    for (let i = 0; i < team1.length + team2.length; i++) {
-        let unitTeam1 = team1[idxTeam1++]
-        if (unitTeam1 && team2) {
-            unitTeam1.takeATurn(team1, team2)
-
-            team2.filter(value => !value.isAlive()).forEach(value => {
-                let indexToRemove = team2.indexOf(value);
-                team2.splice(indexToRemove, 1)
-            })
-        }
-
-
-        let unitTeam2 = team2[idxTeam2++]
-        if (unitTeam2 && team1) {
-            unitTeam2.takeATurn(team2, team1)
-
-            team1.filter(value => !value.isAlive()).forEach(value => {
-                let indexToRemove = team1.indexOf(value);
-                team1.splice(indexToRemove, 1)
-            })
-        }
+function printTeam(team, borderColor, backgroundColor) {
+    for (let i = 0; i < team.length; i++) {
+        let member = team[i]
+        console.log(boxen(`HP:${member.hp}\nultimate:${member.ultimateProgress*100}/100`, {
+            title: member.type,
+            width: 20,
+            borderStyle: "bold",
+            borderColor: borderColor,
+            backgroundColor: backgroundColor
+        }))
     }
 }
 
+function recognizeAnswer(allActions, message) {
+    return allActions.find(value => value.message === message)
+}
 
+let nextUnitTurn = 0;
+let nextEnemyUnitTurn = 0;
+
+const SUPPORTED_COMMAND_ACTIONS = {
+    ATTACK: 'attack',
+    ULTIMATE: 'ultimate'
+}
+
+function createActionsForUnit(unit) {
+    let actions = []
+    for (let i = 0; i < team2.length; i++) {
+        actions.push({action: SUPPORTED_COMMAND_ACTIONS.ATTACK, id: i, message: `Attack enemy unit ${i}(${team2[i].type})`})
+    }
+    if (unit.isUltimateReady()) {
+        actions.push({action: SUPPORTED_COMMAND_ACTIONS.ULTIMATE, message: "Ultimate"})
+    }
+    return actions;
+}
+
+while (team1.length && team2.length) {
+    // console.clear()
+
+    printTeam(team1, "#019152", "#163326")
+    printTeam(team2, "#a30707", "#611919")
+
+    let unit = team1[nextUnitTurn]
+
+    let actions = createActionsForUnit(unit)
+
+    let answer = await inquirer.prompt({
+            name: "def_ask",
+            type: 'rawlist',
+            message: `Unit ${nextUnitTurn}(${unit.type}) action`, choices: actions.map(value => value.message)
+        }
+    )
+
+    let selectedAction = recognizeAnswer(actions, answer['def_ask'])
+    if (selectedAction.action === SUPPORTED_COMMAND_ACTIONS.ATTACK) {
+        unit.doDamage(team2[selectedAction.id])
+    }
+
+    nextUnitTurn++
+}
 
 
 
